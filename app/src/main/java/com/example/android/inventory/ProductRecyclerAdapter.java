b86693f3bbce01464021d96a9e5000ad6a06154a
Ragheed Ma'rouf Huneineh <ragheedmhuneineh@outlook.com>
2017-05-11 00:05:16 +0200

    Add code for recycler adapter

--- app/src/main/java/com/example/android/inventory/ProductRecyclerAdapter.java
+++ app/src/main/java/com/example/android/inventory/ProductRecyclerAdapter.java
@@ -1,10 +1,16 @@
 package com.example.android.inventory;
 
-import android.content.Context;
 import android.database.Cursor;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.android.inventory.data.ProductContract;
 
 /**
  * Created by rmhuneineh on 10/05/2017.
@@ -12,26 +18,85 @@ import android.view.ViewGroup;
 
 public class ProductRecyclerAdapter extends RecyclerView.Adapter<ProductRecyclerAdapter.ViewHolder> {
 
-    public ProductRecyclerAdapter(Context context, Cursor c) {
-        
+    private Cursor mCursor;
+    private CatalogActivity mContext;
+
+    public ProductRecyclerAdapter(CatalogActivity context, Cursor c) {
+        this.mContext = context;
+        this.mCursor = c;
     }
 
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
+        protected TextView nameTextView;
+        protected TextView priceTextView;
+        protected TextView quantityTextView;
+        protected ImageView buy;
+        protected ImageView productPicture;
+
         public ViewHolder(View itemView) {
             super(itemView);
+            nameTextView = (TextView) itemView.findViewById(R.id.text_product_name);
+            priceTextView = (TextView) itemView.findViewById(R.id.text_product_price);
+            quantityTextView = (TextView) itemView.findViewById(R.id.text_product_quantity);
+            buy = (ImageView) itemView.findViewById(R.id.buy);
+            productPicture = (ImageView) itemView.findViewById(R.id.product_image);
         }
     }
 
     @Override
     public ProductRecyclerAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View listItem = LayoutInflater.from(mContext).inflate(R.layout.list_item, parent, false);
+
+        ViewHolder vh = new ViewHolder(listItem);
+        return vh;
     }
 
     @Override
-    public void onBindViewHolder(ProductRecyclerAdapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(ViewHolder holder, int position) {
 
+        final long id;
+        final int mQuantity;
+
+        id = mCursor.getLong(mCursor.getColumnIndex(ProductContract.ProductEntry._ID));
+        int nameColumnIndex = mCursor.getColumnIndex(ProductContract.ProductEntry.COLUMN_PRODUCT_NAME);
+        int priceColumnIndex = mCursor.getColumnIndex(ProductContract.ProductEntry.COLUMN_PRODUCT_PRICE);
+        int quantityColumnIndex = mCursor.getColumnIndex(ProductContract.ProductEntry.COLUMN_PRODUCT_QUANTITY);
+        int pictureColumnIndex = mCursor.getColumnIndex(ProductContract.ProductEntry.COLUMN_PRODUCT_PICTURE);
+
+        String productName = mCursor.getString(nameColumnIndex);
+        String productPrice = mCursor.getString(priceColumnIndex);
+        int quantity = mCursor.getInt(quantityColumnIndex);
+        String imageUriString = mCursor.getString(pictureColumnIndex);
+        Uri imageUri = Uri.parse(imageUriString);
+
+        mQuantity = quantity;
+
+        holder.nameTextView.setText(productName);
+        holder.priceTextView.setText(productPrice);
+        holder.quantityTextView.setText(String.valueOf(quantity));
+        holder.productPicture.setImageURI(imageUri);
+        holder.productPicture.invalidate();
+
+        holder.nameTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mContext.onItemClick(id);
+            }
+        });
+
+        holder.buy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mQuantity >0 ) {
+                    mContext.onBuyClick(id, mQuantity);
+                } else {
+                    Toast.makeText(mContext, "Quantity Unavailable", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
     }
 
     @Override
