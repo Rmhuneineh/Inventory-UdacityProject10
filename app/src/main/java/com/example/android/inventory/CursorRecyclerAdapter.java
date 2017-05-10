dfbfe8d8acd5ef25ba00df72e636ad785347a97b
Ragheed Ma'rouf Huneineh <ragheedmhuneineh@outlook.com>
2017-05-11 00:35:59 +0200

    Switch to abstract class

--- app/src/main/java/com/example/android/inventory/CursorRecyclerAdapter.java
+++ app/src/main/java/com/example/android/inventory/CursorRecyclerAdapter.java
@@ -2,44 +2,47 @@ package com.example.android.inventory;
 
 import android.database.Cursor;
 import android.database.DataSetObserver;
-import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.example.android.inventory.data.ProductContract;
 
 /**
  * Created by rmhuneineh on 10/05/2017.
  */
 
-public class CursorRecyclerAdapter extends RecyclerView.Adapter<CursorRecyclerAdapter.ViewHolder> {
+public abstract class CursorRecyclerAdapter<VH extends RecyclerView.ViewHolder> extends RecyclerView.Adapter<VH> {
 
-    private Cursor mCursor;
-    private CatalogActivity mContext;
+    private Cursor cur;
+    private CatalogActivity ac;
     private boolean mDataValid;
     private int mRowIdColumn;
     private DataSetObserver mDataSetObserver;
 
     public CursorRecyclerAdapter(CatalogActivity context, Cursor c) {
-        this.mContext = context;
-        this.mCursor = c;
-        this.mDataValid = mCursor != null;
-        mRowIdColumn = mDataValid ? mCursor.getColumnIndex("_id") : -1;
+        this.ac = context;
+        this.cur = c;
+        this.mDataValid = cur != null;
+        mRowIdColumn = mDataValid ? cur.getColumnIndex("_id") : -1;
         mDataSetObserver = new NotifyingDataSetObserver();
-        if (mCursor != null) {
-            mCursor.registerDataSetObserver(mDataSetObserver);
+        if (cur != null) {
+            cur.registerDataSetObserver(mDataSetObserver);
         }
+    }
+
+    public Cursor getCursor() {
+        return cur;
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mDataValid && cur != null) {
+            return cur.getCount();
+        }
+        return 0;
     }
 
     @Override
     public long getItemId(int position) {
-        if (mDataValid && mCursor != null && mCursor.moveToPosition(position)) {
-            return mCursor.getLong(mRowIdColumn);
+        if (mDataValid && cur != null && cur.moveToPosition(position)) {
+            return cur.getLong(mRowIdColumn);
         }
         return 0;
     }
@@ -50,95 +53,21 @@ public class CursorRecyclerAdapter extends RecyclerView.Adapter<CursorRecyclerAd
     }
 
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-
-        protected TextView nameTextView;
-        protected TextView priceTextView;
-        protected TextView quantityTextView;
-        protected ImageView buy;
-        protected ImageView productPicture;
-
-        public ViewHolder(View itemView) {
-            super(itemView);
-            nameTextView = (TextView) itemView.findViewById(R.id.text_product_name);
-            priceTextView = (TextView) itemView.findViewById(R.id.text_product_price);
-            quantityTextView = (TextView) itemView.findViewById(R.id.text_product_quantity);
-            buy = (ImageView) itemView.findViewById(R.id.buy);
-            productPicture = (ImageView) itemView.findViewById(R.id.product_image);
-        }
-    }
+    public abstract void onBindViewHolder(VH viewHolder, Cursor cursor);
 
     @Override
-    public CursorRecyclerAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View listItem = LayoutInflater.from(mContext).inflate(R.layout.list_item, parent, false);
-
-        ViewHolder vh = new ViewHolder(listItem);
-        return vh;
-    }
-
-    @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public void onBindViewHolder(VH holder, int position) {
         if (!mDataValid) {
             throw new IllegalStateException("this should only be called when the cursor is valid");
         }
-        if (!mCursor.moveToPosition(position)) {
+        if (!cur.moveToPosition(position)) {
             throw new IllegalStateException("couldn't move cursor to position " + position);
         }
 
+        onBindViewHolder(holder, cur);
 
-        final long id;
-        final int mQuantity;
 
-        id = mCursor.getLong(mCursor.getColumnIndex(ProductContract.ProductEntry._ID));
-        int nameColumnIndex = mCursor.getColumnIndex(ProductContract.ProductEntry.COLUMN_PRODUCT_NAME);
-        int priceColumnIndex = mCursor.getColumnIndex(ProductContract.ProductEntry.COLUMN_PRODUCT_PRICE);
-        int quantityColumnIndex = mCursor.getColumnIndex(ProductContract.ProductEntry.COLUMN_PRODUCT_QUANTITY);
-        int pictureColumnIndex = mCursor.getColumnIndex(ProductContract.ProductEntry.COLUMN_PRODUCT_PICTURE);
 
-        String productName = mCursor.getString(nameColumnIndex);
-        String productPrice = mCursor.getString(priceColumnIndex);
-        int quantity = mCursor.getInt(quantityColumnIndex);
-        String imageUriString = mCursor.getString(pictureColumnIndex);
-        Uri imageUri = Uri.parse(imageUriString);
-
-        mQuantity = quantity;
-
-        holder.nameTextView.setText(productName);
-        holder.priceTextView.setText(productPrice);
-        holder.quantityTextView.setText(String.valueOf(quantity));
-        holder.productPicture.setImageURI(imageUri);
-        holder.productPicture.invalidate();
-
-        holder.nameTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mContext.onItemClick(id);
-            }
-        });
-
-        holder.buy.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(mQuantity >0 ) {
-                    mContext.onBuyClick(id, mQuantity);
-                } else {
-                    Toast.makeText(mContext, "Quantity Unavailable", Toast.LENGTH_SHORT).show();
-                }
-
-            }
-        });
-    }
-
-    @Override
-    public int getItemCount() {
-        if (mDataValid && mCursor != null) {
-            return mCursor.getCount();
-        }
-        return 0;
-    }
-
-    public Cursor getCursor() {
-        return mCursor;
     }
 
     public void changeCursor(Cursor cursor) {
@@ -149,17 +78,17 @@ public class CursorRecyclerAdapter extends RecyclerView.Adapter<CursorRecyclerAd
     }
 
     public Cursor swapCursor(Cursor newCursor) {
-        if (newCursor == mCursor) {
+        if (newCursor == cur) {
             return null;
         }
-        final Cursor oldCursor = mCursor;
+        final Cursor oldCursor = cur;
         if (oldCursor != null && mDataSetObserver != null) {
             oldCursor.unregisterDataSetObserver(mDataSetObserver);
         }
-        mCursor = newCursor;
-        if (mCursor != null) {
+        cur = newCursor;
+        if (cur != null) {
             if (mDataSetObserver != null) {
-                mCursor.registerDataSetObserver(mDataSetObserver);
+                cur.registerDataSetObserver(mDataSetObserver);
             }
             mRowIdColumn = newCursor.getColumnIndexOrThrow("_id");
             mDataValid = true;
